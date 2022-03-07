707c726e6907891ea201f6db5d4cb71f4c42d6f9
anton <ashtanyuk@gmail.com>
2022-03-07 08:32:41 +0300

    Начальный коммит

---
+++ src/alg.cpp
@@ -0,0 +1,10 @@
+// Copyright 2021 NNTU-CS
+int countPairs1(int *arr, int len, int value) {
+  return 0;
+}
+int countPairs2(int *arr, int len, int value) {
+  return 0;
+}
+int countPairs3(int *arr, int len, int value) {
+  return 0;
+}

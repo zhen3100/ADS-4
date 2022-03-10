a97ef28319b38366df4eac0c22e509f2ccd1d881
Evgeny Volkov <88347990+zhen3100@users.noreply.github.com>
2022-03-10 14:00:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,71 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int binSearch(int *arr, int left, int right, int number) {
+    if (right >= left) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == number)
+            return binSearch(arr,left,mid-1,number)+binSearch(arr,mid+1,right,number)+1;
+        else if (arr[mid] > number)
+            return binSearch(arr, left, mid-1, number);
+        return binSearch(arr, mid + 1, right, number);
+    }
+    return 0;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+
+int countPairs1(int* arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+
+            }
+        }
+    }
+    return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+int countPairs2(int* arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = len-1; j > i; j--) {
+
+            if (arr[i]+arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
+
+}
+int countPairs3(int* arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        count += binSearch(&arr[i+1], 0, len-i, value - arr[i]);
+    }
+    return count;
+}
+
+
+void quickSort(int* array, int low, int high) {
+    int i = low;
+    int j = high;
+    int pivot = array[(i + j) / 2];
+    int temp;
+
+    while (i <= j) {
+        while (array[i] < pivot)
+            i++;
+        while (array[j] > pivot)
+            j--;
+        if (i <= j) {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    if (j > low)
+        quickSort(array, low, j);
+    if (i < high)
+        quickSort(array, i, high);
 }
